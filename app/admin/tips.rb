ea307fecdac2253cfbd5b5ce1a80d31dc01b6fb0
carolina <carolina.santiago.t@hotmail.com>
2021-01-07 09:54:26 +0000

    Tip

---
+++ app/admin/tips.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Tip do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :tip_description, :user_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:tip_description, :user_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
