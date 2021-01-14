0750edae549100cdf2a5d9c794de13a1ea0c40c5
carolina <carolina.santiago.t@hotmail.com>
2021-01-14 10:31:03 +0000

    new tip form alteration

--- app/admin/tips.rb
+++ app/admin/tips.rb
@@ -11,19 +11,12 @@ ActiveAdmin.register Tip do
     end
   end 
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :tip_description, :user_id
-  #
-  # or
-  #
-  #permit_params do
-  #  permitted = [:tip_description, :user_id]
-  #  permitted << :other if params[:action] == 'create' && current_user.admin? 
-  #  permitted
-  #end
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :tip_description
+    end
+    f.actions
+  end
   
 end
